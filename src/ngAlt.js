92784452df2b30bffec2aab8d44e79cb2e806457
Artem I <2urnix@gmail.com>
2016-04-10 13:09:25 +0300

    feat(ngAlt): ng-alt-size option added

--- src/ngAlt.js
+++ src/ngAlt.js
@@ -1,15 +1,18 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular.module('ngAlt', [])
-        .directive('ngAlt', [
-            function () {
-                return {
-                    link: function (scope, element, attrs) {
-                        if (!attrs.ngSrc && attrs.ngAlt)
-                            attrs.$set('data-src', 'holder.js/100px500?text=' + attrs.ngAlt);
-                        Holder.run({images: element[0]});
-                    }
-                };
-            }]);
+  angular.module('ngAlt', [])
+    .directive('ngAlt', [
+      function () {
+        return {
+          link: function (scope, element, attrs) {
+            if (!attrs.ngSrc && attrs.ngAlt) {
+              var text = attrs.ngAlt;
+              var size = attrs.ngAltSize ? attrs.ngAltSize : '100px500';
+              attrs.$set('data-src', 'holder.js/' + size + '?text=' + text);
+            }
+            Holder.run({images: element[0]});
+          }
+        };
+      }]);
 })();
