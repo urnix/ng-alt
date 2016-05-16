7b0b0a972b6ac3f81366230b9f6b2cb4cce8f974
Artem I <2urnix@gmail.com>
2016-05-16 16:44:40 +0300

    fix(src/ngAlt): fix bug with replacing `src` on resize

--- src/ngAlt.js
+++ src/ngAlt.js
@@ -23,14 +23,16 @@
 
         function applyHolder(element, attrs) {
           // console.log('catch!');
-          
+
           //noinspection JSUnresolvedVariable
           var text = attrs.ngAlt;
           //noinspection JSUnresolvedVariable
           var size = attrs.ngAltSize ? attrs.ngAltSize : '100px500';
           attrs.$set('data-src', 'holder.js/' + size + '?text=' + text);
           //noinspection JSUnresolvedVariable
-          Holder.run({images: element[0]});
+          var image = element[0];
+          Holder.run({images: image});
+          Holder.setResizeUpdate(image, false);
         }
       }]);
 })();
